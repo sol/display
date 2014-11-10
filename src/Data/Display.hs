bc9f62a745e99b3e3e24f46a9eb7aca59f3ff104
Simon Hengel <sol@typeful.net>
2014-11-11 01:37:05 +0800

    Add Data.Display.Lazy

--- src/Data/Display.hs
+++ src/Data/Display.hs
@@ -1,38 +1,12 @@
-{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, DefaultSignatures #-}
-module Data.Display where
+module Data.Display (
+  Display (..)
+, display
+) where
 
 import           Data.Text (Text)
-import           Data.Text.Lazy.Builder
 import           Data.Text.Lazy (toStrict)
-import qualified Data.Text.Lazy as LT
-import           Data.Text.Lazy.Builder.Int (decimal)
-import           Data.Text.Lazy.Builder.RealFloat
-
-class Display a where
-  displayBuilder :: a -> Builder
-  default displayBuilder :: Show a => a -> Builder
-  displayBuilder = fromString . show
+import           Data.Display.Lazy (Display(..))
+import qualified Data.Display.Lazy as Lazy
 
 display :: Display a => a -> Text
-display = toStrict . toLazyText . displayBuilder
-
-instance Display String where
-  displayBuilder = fromString
-
-instance Display Text where
-  displayBuilder = fromText
-
-instance Display LT.Text where
-  displayBuilder = fromLazyText
-
-instance Display Int where
-  displayBuilder = decimal
-
-instance Display Integer where
-  displayBuilder = decimal
-
-instance Display Float where
-  displayBuilder = realFloat
-
-instance Display Double where
-  displayBuilder = realFloat
+display = toStrict . Lazy.display
